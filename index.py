b25c6f46d7f65cca9f22e8cbe47cb65a1482a1f3
csae8092 <peter.andorfer@oeaw.ac.at>
2024-05-22 12:26:30 +0200

    added index script; ToDo; add to GH-Action

---
+++ index.py
@@ -0,0 +1,78 @@
+import json
+import pandas as pd
+from slugify import slugify
+
+from config import (
+    EDITIONS,
+    INSTITUTIONS,
+    TS_CLIENT,
+    TS_SCHEMA_NAME,
+    MANDATORY_FIELDS,
+    FACET_FIELDS,
+    NO_FACET_FIELDS,
+)
+
+from utils import (
+    make_schema,
+    delete_and_create_schema,
+)
+
+print("hallo, lets start indexing")
+
+print(f"fetching {EDITIONS}")
+df = pd.read_csv(EDITIONS).head(-1)
+df = df.astype("str")
+df = df.replace(["nan", ""], "not provided")
+objects = df.to_dict(orient="records")
+labels = {slugify(x): x for x in df.keys()}
+df.columns = labels.keys()
+editions = df.to_dict(orient="records")
+
+print(f"fetching {INSTITUTIONS}")
+df = pd.read_csv(INSTITUTIONS).head(-1)
+df = df.astype("str")
+df = df.replace(["nan", ""], "not provided")
+objects = df.to_dict(orient="records")
+labels = {slugify(x): x for x in df.keys()}
+df.columns = labels.keys()
+orgs = {}
+for i, x in enumerate(df.to_dict(orient="records"), start=1):
+    orgs[x["institution-name"]] = x
+    orgs[x["institution-name"]]["id"] = f"{i}"
+
+editions_full = []
+for i, x in enumerate(editions, start=1):
+    x["id"] = f"{i}"
+    old_orgs = [x.strip() for x in x["institution-s"].split(";")]
+    institutions = []
+    for org in old_orgs:
+        try:
+            institutions.append(orgs[org])
+        except KeyError:
+            continue
+    x["institution-s"] = institutions
+    for f in FACET_FIELDS:
+        try:
+            x[f] = [x.strip() for x in x[f].split(";")]
+        except AttributeError:
+            continue
+    editions_full.append(x)
+
+with open("editions.json", "w") as f:
+    json.dump(editions_full, f, ensure_ascii=False, indent=4)
+
+
+print("Typesense Index")
+print(f"defining Typesense collection schema with name: {TS_SCHEMA_NAME}")
+ts_schema = make_schema(editions[0], "dig-ed-cat", MANDATORY_FIELDS, NO_FACET_FIELDS)
+with open("schema.json", "w") as f:
+    json.dump(ts_schema, f, ensure_ascii=False, indent=4)
+
+print(f"creating Typesnses Collection with schema {TS_SCHEMA_NAME}")
+cur_schema = delete_and_create_schema(TS_CLIENT, TS_SCHEMA_NAME, ts_schema)
+print(f"schema created at {cur_schema['created_at']}")
+print(f"start to index {len(editions_full)} records")
+index = TS_CLIENT.collections[TS_SCHEMA_NAME].documents.import_(
+    editions_full, {"action": "create"}
+)
+print(f"status of index-process: {index[0]}")
---
+++ utils.py
@@ -0,0 +1,42 @@
+""" some helper functions """
+import typesense
+
+
+def make_schema(record, ts_schema_name, mandatory_fields, no_facet_field):
+    TS_SCHEMA = {"name": ts_schema_name, "enable_nested_fields": True, "fields": []}
+    for key, value in record.items():
+        if key in mandatory_fields:
+            TS_SCHEMA["fields"].append({"name": key, "type": "string", "sort": True})
+        elif key in no_facet_field:
+            TS_SCHEMA["fields"].append(
+                {
+                    "name": key,
+                    "type": "string",
+                    "facet": False,
+                    "optional": True,
+                    "sort": True,
+                }
+            )
+        elif isinstance(value, list):
+            if isinstance(value[0], dict):
+                TS_SCHEMA["fields"].append(
+                    {"name": key, "type": "object[]", "facet": True, "optional": True}
+                )
+            else:
+                TS_SCHEMA["fields"].append(
+                    {"name": key, "type": "string[]", "facet": True, "optional": True}
+                )
+        else:
+            TS_SCHEMA["fields"].append(
+                {"name": key, "type": "string", "facet": True, "optional": True}
+            )
+    return TS_SCHEMA
+
+
+def delete_and_create_schema(ts_client, ts_schema_name, ts_schema):
+    try:
+        ts_client.collections[ts_schema_name].delete()
+    except typesense.api_call.ObjectNotFound:
+        pass
+    created = ts_client.collections.create(ts_schema)
+    return created
