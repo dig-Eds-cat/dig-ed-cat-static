9fd3beb82f383f5b81424b1cbc98ba489dce0a10
csae8092 <p.andorfer@gmail.com>
2025-02-27 11:45:36 +0100

    chore: fetch data from new source repo

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 import typesense
 
-GITHUB_BASE = "https://raw.githubusercontent.com/gfranzini/digEds_cat/master"
+GITHUB_BASE = "https://raw.githubusercontent.com/dig-Eds-cat/digEds_cat/refs/heads/main/"
 
 EDITIONS = f"{GITHUB_BASE}/digEds_cat.csv"
 INSTITUTIONS = f"{GITHUB_BASE}/institutions_places_enriched.csv"
