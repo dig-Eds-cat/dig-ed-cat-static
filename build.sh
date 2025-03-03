648084a3eb7bab05ed2f478caf00bd34a77a136e
csae8092 <p.andorfer@gmail.com>
2025-03-03 10:02:58 +0100

    wip

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-python build__editiondata.py
-python build__institutiondata.py
-python build__handles.py
-python build__index.py
+# python build__editiondata.py
+# python build__institutiondata.py
+# python build__handles.py
+# python build__index.py
 python build__static.py
 python build__dynamic.py
